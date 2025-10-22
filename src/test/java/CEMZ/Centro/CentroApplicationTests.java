9b201544a51dfcfba9ebf90c953622af8d1f235e
Tomybuc <bucolotomas@gmail.com>
2025-10-22 10:42:05 -0300

    Re-proyecto-cemz
    Recolocando el proyecto cemz

---
+++ src/test/java/CEMZ/Centro/CentroApplicationTests.java
@@ -0,0 +1,13 @@
+package CEMZ.Centro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
